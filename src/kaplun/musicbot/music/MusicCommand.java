05df9442d59a81c3dbfa1288b84d53334cc12988
kaplun22 <kaplun22@hotmail.com>
2018-01-21 13:49:52 +0200

    Added pause command

--- src/kaplun/musicbot/music/MusicCommand.java
+++ src/kaplun/musicbot/music/MusicCommand.java
@@ -39,6 +39,9 @@ public class MusicCommand {
 
     @Command(name = "skip", type = Command.ExecutorType.USER)
     private void skip(Guild guild, TextChannel textChannel, User user) {
+
+
+
         manager.getPlayer(guild).skipTrack();
         textChannel.sendMessage("track skipped.").queue();
     }
@@ -57,12 +60,63 @@ public class MusicCommand {
     }
 
 
+    @Command(name = "МинусУши", type = Command.ExecutorType.USER)
+    private void noEars(Guild guild, TextChannel textChannel, User user, String command) {
+
+
+        String author = guild.getMember(user).toString();
+        String[] getNickname = author.split(":");
+        String[] nickname = getNickname[1].split(" ");
+
+
+        System.out.println(author);
+        System.out.println(nickname[0]);
+
+        if (guild == null) return;
+
+        if (author.contains("kaplun")) {
+            if (!guild.getAudioManager().isConnected() && !guild.getAudioManager().isAttemptingToConnect()) {
+                VoiceChannel voiceChannel = guild.getMember(user).getVoiceState().getChannel();
+                if (voiceChannel == null) {
+                    textChannel.sendMessage("Bot connected.").queue();
+                    return;
+                }
+                guild.getAudioManager().openAudioConnection(voiceChannel);
+            }
+
+
+            manager.loadTrack(textChannel, "https://www.youtube.com/watch?v=EKjkUvlm7Ng");
+        } else {
+            textChannel.sendMessage(nickname + " ПОШЕЛ НАХУЙ").queue();
+        }
+    }
+
+
+    @Command(name = "MLG", type = Command.ExecutorType.USER)
+    private void MLG(Guild guild, TextChannel textChannel, User user, String command) {
+
+        if (guild == null) return;
+
+
+        if (!guild.getAudioManager().isConnected() && !guild.getAudioManager().isAttemptingToConnect()) {
+            VoiceChannel voiceChannel = guild.getMember(user).getVoiceState().getChannel();
+            if (voiceChannel == null) {
+                textChannel.sendMessage("Bot connected.").queue();
+                return;
+            }
+            guild.getAudioManager().openAudioConnection(voiceChannel);
+        }
+
+        manager.loadTrack(textChannel, "https://www.youtube.com/watch?v=Tqo-v-zeZ24");
+    }
+
+
     @Command(name = "search", type = Command.ExecutorType.USER)
     private void searchForVideos(Guild guild, TextChannel textChannel, User user, String command) throws Exception {
 
         Document doc = Jsoup.connect("https://www.google.com/search?q=" + command).userAgent("Mozilla/5.0").timeout(5000).get();
 
-        Element el = doc.getElementsByAttributeValue("class", "kv").first();
+        Element el = doc.getElementsByAttributeValue("class","kv").first();
 
 
         if (guild == null) return;
@@ -81,35 +135,51 @@ public class MusicCommand {
 
     }
 
-    @Command(name = "summon", type = Command.ExecutorType.USER)
-    private void summon(Guild guild, TextChannel textChannel, User user, String command) {
+    @Command(name="summon",type = Command.ExecutorType.USER)
+    private void summon (Guild guild, TextChannel textChannel, User user ,String command){
         VoiceChannel voiceChannel = guild.getMember(user).getVoiceState().getChannel();
         guild.getAudioManager().openAudioConnection(voiceChannel);
     }
 
 
+
     @Command(name = "disconnect", type = Command.ExecutorType.USER)
-    private void disconnect(Guild guild, TextChannel textChannel, User user, String command) {
+    private  void disconnect (Guild guild ,TextChannel textChannel , User user,String command){
         guild.getAudioManager().closeAudioConnection();
     }
-
-    @Command(name = "vol", type = Command.ExecutorType.USER)
-    private void volume(Guild guild, TextChannel textChannel, User user, String command) {
+    @Command(name="vol",type =Command.ExecutorType.USER)
+    private void volume (Guild guild ,TextChannel textChannel , User user,String command){
         String[] vol = command.split(" ");
         int volu = Integer.parseInt(vol[1]);
         manager.getPlayer(guild).getAudioPlayer().setVolume(volu);
     }
 
-    @Command(name = "pause", type = Command.ExecutorType.USER)
-    private void pause(Guild guild, TextChannel textChannel, User user, MusicPlayer player, String command) {
-        manager.getPlayer(guild).getAudioPlayer().setPaused(true);
+    @Command(name="pause",type =Command.ExecutorType.USER)
+    private void pause (Guild guild ,TextChannel textChannel , User user,MusicPlayer player,String command){
+      manager.getPlayer(guild).getAudioPlayer().setPaused(true);
     }
-
-    @Command(name = "unpause", type = Command.ExecutorType.USER)
-    private void unPause(Guild guild, TextChannel textChannel, User user, MusicPlayer player, String command) {
+    @Command(name="unpause",type =Command.ExecutorType.USER)
+    private void unPause (Guild guild ,TextChannel textChannel , User user,MusicPlayer player,String command){
         manager.getPlayer(guild).getAudioPlayer().setPaused(false);
     }
+
+    @Command(name = "испанцы", type = Command.ExecutorType.USER)
+    private void ispanci (Guild guild, TextChannel textChannel, User user, String command) {
+
+        if (guild == null) return;
+
+
+        if (!guild.getAudioManager().isConnected() && !guild.getAudioManager().isAttemptingToConnect()) {
+            VoiceChannel voiceChannel = guild.getMember(user).getVoiceState().getChannel();
+            if (voiceChannel == null) {
+                textChannel.sendMessage("Bot connected.").queue();
+                return;
+            }
+            guild.getAudioManager().openAudioConnection(voiceChannel);
+        }
+
+        manager.loadTrack(textChannel, "https://www.youtube.com/watch?v=BU2NWMh9y7E");
+    }
+
 }
-
-
 
